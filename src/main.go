b94ef9b73392f8301dbc0af6582c4a5da2f6c12b
Akshay Siv <46586248+AkshaySiv@users.noreply.github.com>
2025-05-13 17:56:41 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -6,12 +6,19 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	// Serve files from the "static" directory.
+	// This will handle all requests and serve static files relative to the "./static" path.
+	fileServer := http.FileServer(http.Dir("./static"))
 
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
+	// Handle root URL "/" by serving files using the fileServer handler.
+	http.Handle("/", fileServer)
+
+	// Define the port the server will listen on.
+	port := ":8080"
+	log.Printf("Starting server on http://localhost%s\n", port)
+
+	// Start the HTTP server and log any fatal errors.
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
